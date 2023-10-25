7bd5c9564b7234deebfed3b46718e20574f34ddc
Seungri-github <144909697+Seungri-github@users.noreply.github.com>
2023-10-26 03:52:47 +0900

    Update Homework3.java

--- Homework3.java
+++ Homework3.java
@@ -1,30 +1,22 @@
 import java.util.Scanner;
 public class Homework3 {
-    public static void main(String[] args) {
-        System.out.printf("몇 개의 수를 입력할 예정인가요? ");
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.printf("몇 개의 정수를 입력할 예정인가요?: ");
         int num = sc.nextInt();
-        int[] arr = new int[num];
+        int arr[] = new int[num];
 
-        System.out.printf("수를 입력하세요: ");
-        for(int i = 0; i < arr.length; i++) {
+        System.out.print("수를 입력하세요: ");
+        for (int i=0; i<num; i++) {
             arr[i] = sc.nextInt();
         }
-
         int max = arr[0];
         int min = arr[0];
-
-        for(int i = 0; i < arr.length; i++) {
-            if(max < arr[i]) {
-                max = arr[i];
-            }
-            if(min > arr[i]) {
-                min = arr[i];
-            }
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
         }
-
         System.out.println("최대값: " + max);
         System.out.println("최소값: " + min);
-
     }
 }
